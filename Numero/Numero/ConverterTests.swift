7ee0083171da046080bb337d508dfa1a94b14f0f
chrisabbod <chrisabbod@gmail.com>
2019-01-08 18:29:55 -0500

    Add test case for special case 3999

--- Numero/Numero/ConverterTests.swift
+++ Numero/Numero/ConverterTests.swift
@@ -64,5 +64,14 @@ class ConverterTests: XCTestCase {
         XCTAssertEqual(result, "IX", "Conversion for 9 is incorrect")
     }
 
+    func testConverstionForZero() {
+        let result = converter.convert(0)
+        XCTAssertEqual(result, "", "Conversion for 0 is incorrect")
+    }
+
+    func testConverstionFor3999() {
+        let result = converter.convert(3999)
+        XCTAssertEqual(result, "MMMCMXCIX", "Conversion for 3999 is incorrect")
+    }
 
 }

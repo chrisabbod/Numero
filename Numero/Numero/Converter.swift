11862de79f1538b46c132c537f0aa122b3622e1b
chrisabbod <chrisabbod@gmail.com>
2019-01-08 17:59:10 -0500

    Add tests for 1, 2, 3, & 5 conversion

--- Numero/Numero/Converter.swift
+++ Numero/Numero/Converter.swift
@@ -28,4 +28,12 @@
 
 class Converter {
   
+    func convert(_ number: Int) -> String {
+        if number == 5 {
+            return "V"
+        } else {
+            return String(repeating: "I", count: number)
+        }
+    }
+
 }
---
+++ Numero/Numero/ConverterTests.swift
@@ -0,0 +1,39 @@
+//
+//  ConverterTests.swift
+//  NumeroTests
+//
+//  Created by Chris Abbod on 1/8/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import XCTest
+
+@testable import Numero
+
+class ConverterTests: XCTestCase {
+
+    let converter = Converter()
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testConversionForOne(){
+        let result = converter.convert(1)
+        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+    }
+    
+    func testConversionForTwo(){
+        let result = converter.convert(2)
+        XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
+    }
+    
+    func testConversionForFive(){
+        let result = converter.convert(5)
+        XCTAssertEqual(result, "V", "Conversion for 5 is incorrect")
+    }
+}
